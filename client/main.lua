84a91d97dfdf8d7eb9a9e89583d81542240aea4f
Silvio Heuberger <silvio.heuberger@beekeeper.io>
2020-04-02 14:21:44 +0200

    Reformat files using the formatter

--- client/main.lua
+++ client/main.lua
@@ -14,229 +14,196 @@ local inspect = require("lib.inspect")
 local suit = require("lib.suit")
 
 -- connection states
-local broadcasting = {
-	port = 9020
-}
+local broadcasting = {port = 9020}
 
-local connecting = {
-	port = 9022
-}
+local connecting = {port = 9022}
 
-local connected = {
-	stats = {
-		time_update = 0,
-		time_draw = 0
-	}
-}
+local connected = {stats = {time_update = 0, time_draw = 0}}
 
 local Mfd = require("mfd")
 
-local connection = {
-	ip = nil,
-	server = nil,
-	host = nil,
-	peer = nil
-}
+local connection = {ip = nil, server = nil, host = nil, peer = nil}
 
-local shine = {
-	dots = { ".", "..", "..." },
-	position = 1
-}
+local shine = {dots = {".", "..", "..."}, position = 1}
 
-local state = {
-	debug = true
-}
+local state = {debug = true}
 
 local components = {}
 
 local channels = {
-    -- general purpose reliable channel
-    [0] = function(event)
-        local payload = msgpack.unpack(event.data)
-        print("Received general purpose event: ", inspect(payload))
-    end,
-
-    -- texture memory channels are unrealiable and pure image data.
-    [1] = function(event)
-        components["f16/left-mfd"]:consume(event.data)
-    end,
-    [2] = function(event)
-        components["f16/right-mfd"]:consume(event.data)
-    end,
-    [3] =  function(event)
-        components["f16/ded"]:consume(event.data)
-    end,
-    [4] = function(event)
-        components["f16/rwr"]:consume(event.data)
-    end
+  -- general purpose reliable channel
+  [0] = function(event)
+    local payload = msgpack.unpack(event.data)
+    print("Received general purpose event: ", inspect(payload))
+  end,
+  -- texture memory channels are unrealiable and pure image data.
+  [1] = function(event)
+    components["f16/left-mfd"]:consume(event.data)
+  end,
+  [2] = function(event)
+    components["f16/right-mfd"]:consume(event.data)
+  end,
+  [3] = function(event)
+    components["f16/ded"]:consume(event.data)
+  end,
+  [4] = function(event)
+    components["f16/rwr"]:consume(event.data)
+  end,
 }
 
 function love.load()
-	love.graphics.setFont(love.graphics.newFont("fonts/falconded.ttf", 20, 'normal'))
-	tick.framerate = 60 -- Limit framerate to 60 frames per second.
-	tick.rate = 0.02 -- 50 updates per second
+  love.graphics.setFont(love.graphics.newFont("fonts/falconded.ttf", 20, "normal"))
+  tick.framerate = 60 -- Limit framerate to 60 frames per second.
+  tick.rate = 0.02 -- 50 updates per second
 
-    State.registerEvents()
-    -- can bypass broadcast, if server ip is known
-	State.switch(connecting, "127.0.0.1")
-	-- State.switch(broadcasting)
+  State.registerEvents()
+  -- can bypass broadcast, if server ip is known
+  State.switch(connecting, "127.0.0.1")
+  -- State.switch(broadcasting)
 
-	Signal.register("send-to-server", function(message)
-		connection.server:send(msgpack.pack(message))
-	end)
+  Signal.register("send-to-server", function(message)
+    connection.server:send(msgpack.pack(message))
+  end)
 
-	Timer.every(0.5, function()
-		shine.position = ((shine.position + 1) % #shine.dots) + 1
-	end)
+  Timer.every(0.5, function()
+    shine.position = ((shine.position + 1) % #shine.dots) + 1
+  end)
 end
 
 function broadcasting:enter()
-	broadcasting.socket = socket.udp4()
-	broadcasting.socket:settimeout(0)
-	broadcasting.socket:setoption("broadcast", true)
-	broadcasting.send = Timer.every(1, self.sendHello)
-	broadcasting.receive = Timer.every(1, self.receiveAck)
+  broadcasting.socket = socket.udp4()
+  broadcasting.socket:settimeout(0)
+  broadcasting.socket:setoption("broadcast", true)
+  broadcasting.send = Timer.every(1, self.sendHello)
+  broadcasting.receive = Timer.every(1, self.receiveAck)
 end
 
 function broadcasting:sendHello()
-	local message = msgpack.pack({
-		type = "hello"
-	})
-	broadcasting.socket:sendto(message, "255.255.255.255", broadcasting.port)
+  local message = msgpack.pack({type = "hello"})
+  broadcasting.socket:sendto(message, "255.255.255.255", broadcasting.port)
 end
 
 function broadcasting:receiveAck()
-	local datagram, ip, port, error = broadcasting.socket:receivefrom()
-	if datagram and ip and port then
-		local message = msgpack.unpack(datagram)
-		if message.type == "ack" then
-			print("Discovered server at [${ip}] on port [${port}]" % { ip = ip, port = port })
-			State.switch(connecting, ip)
-		end
-	end
-end
-
-function broadcasting:leave()
-	Timer.cancel(broadcasting.send)
-	Timer.cancel(broadcasting.receive)
-	broadcasting.socket:close()
-end
-
-function broadcasting:draw()
-	love.graphics.print("DISCOVERING SERVER" .. shine.dots[shine.position], 30, 30)
-end
-
-function connecting:enter(previous, serverIp)
-    connection.ip = serverIp or connection.ip
-    print("Using " .. #channels+1 .. " channels.")
-	connection.host = enet.host_create(nil, nil, #channels+1)
-	connection.server = connection.host:connect(connection.ip .. ":" .. connecting.port)
-end
-
-function connecting:update(dt)
-	local event = connection.host:service()
-	while event do
-		if event.type == "connect" then
-			print("Connected ...")
-			connection.peer = event.peer
-			State.switch(connected)
-		end
-		event = connection.host:service()
-	end
-end
-
-function connecting:draw()
-	love.graphics.print("CONNECTING" .. shine.dots[shine.position], 30, 30)
-end
-
-function connected:init()
-	local leftMfd = Mfd("f16/left-mfd", 20, 30)
-    local rightMfd = Mfd("f16/right-mfd", 520, 30)
-    components[leftMfd.id] = leftMfd
-    components[rightMfd.id] = rightMfd
-end
-
-function connected:update(dt)
-	local t1 = love.timer.getTime()
-
-	for _,component in pairs(components) do
-		component:update(dt);
-	end
-
-	local event = connection.host:service()
-	while event do
-        if event.type == "disconnect" then
-			print("Disconnected.")
-			State.switch(connecting)
-        elseif event.type == "receive" then
-            channels[event.channel](event)
-        end
-		event = connection.host:service()
-	end
-	local t2 = love.timer.getTime()
-	self.stats.time_update = (t2-t1) * 1000
-end
-
-function connected:draw()
-	local t1 = love.timer.getTime()
-
-	love.graphics.setColor(1,1,1)
-	for _,component in pairs(components) do
-		component:draw()
-	end
-
-	local t2 = love.timer.getTime()
-	self.stats.time_draw = (t2-t1) * 1000
-
-	if state.debug then
-		self:draw_debug_info()
-	end
-end
-
-function connected:draw_debug_info()
-	if state.debug then
-		local fps = love.timer.getFPS()
-		local mem = collectgarbage("count")
-		local text = ("upd: %.2fms, drw: %.2fms, fps: %d, mem: %.2fMB, tex_mem: %.2fMB, ping: %.0fms")
-			:format(connected.stats.time_update, connected.stats.time_draw, fps, mem / 1024, love.graphics.getStats().texturememory / 1024 / 1024, connection.server:round_trip_time())
-        
-		love.graphics.setColor(255, 255, 255)
-		love.graphics.printf(text, 10, love.graphics.getHeight() - 20, love.graphics.getWidth(), "left")
-	end
-end
-
-function connected:leave()
-	connection.peer:disconnect()
-	connection.host:flush()
-end
-
-function connected:mousepressed(x, y, button, isTouch, presses)
-	for _,component in pairs(components) do
-		component:mousepressed(x, y, button, isTouch, presses)
-	end
-end
-
-function connected:mousereleased(x, y, button, isTouch, presses)
-	for _,component in pairs(components) do
-		component:mousereleased(x, y, button, isTouch, presses)
-	end
-end
-
-function love.update(dt)
-	-- always update timers
-	Timer.update(dt)
-end
-
-function love.keypressed(key, scancode, isrepeat)
-  if key == "escape" then
-	love.event.quit()
+  local datagram, ip, port, error = broadcasting.socket:receivefrom()
+  if datagram and ip and port then
+    local message = msgpack.unpack(datagram)
+    if message.type == "ack" then
+      print("Discovered server at [${ip}] on port [${port}]" % {ip = ip, port = port})
+      State.switch(connecting, ip)
+    end
   end
 end
 
+function broadcasting:leave()
+  Timer.cancel(broadcasting.send)
+  Timer.cancel(broadcasting.receive)
+  broadcasting.socket:close()
+end
+
+function broadcasting:draw()
+  love.graphics.print("DISCOVERING SERVER" .. shine.dots[shine.position], 30, 30)
+end
+
+function connecting:enter(previous, serverIp)
+  connection.ip = serverIp or connection.ip
+  print("Using " .. #channels + 1 .. " channels.")
+  connection.host = enet.host_create(nil, nil, #channels + 1)
+  connection.server = connection.host:connect(connection.ip .. ":" .. connecting.port)
+end
+
+function connecting:update(dt)
+  local event = connection.host:service()
+  while event do
+    if event.type == "connect" then
+      print("Connected ...")
+      connection.peer = event.peer
+      State.switch(connected)
+    end
+    event = connection.host:service()
+  end
+end
+
+function connecting:draw()
+  love.graphics.print("CONNECTING" .. shine.dots[shine.position], 30, 30)
+end
+
+function connected:init()
+  local leftMfd = Mfd("f16/left-mfd", 20, 30)
+  local rightMfd = Mfd("f16/right-mfd", 520, 30)
+  components[leftMfd.id] = leftMfd
+  components[rightMfd.id] = rightMfd
+end
+
+function connected:update(dt)
+  local t1 = love.timer.getTime()
+
+  for _, component in pairs(components) do component:update(dt) end
+
+  local event = connection.host:service()
+  while event do
+    if event.type == "disconnect" then
+      print("Disconnected.")
+      State.switch(connecting)
+    elseif event.type == "receive" then
+      channels[event.channel](event)
+    end
+    event = connection.host:service()
+  end
+  local t2 = love.timer.getTime()
+  self.stats.time_update = (t2 - t1) * 1000
+end
+
+function connected:draw()
+  local t1 = love.timer.getTime()
+
+  love.graphics.setColor(1, 1, 1)
+  for _, component in pairs(components) do component:draw() end
+
+  local t2 = love.timer.getTime()
+  self.stats.time_draw = (t2 - t1) * 1000
+
+  if state.debug then self:draw_debug_info() end
+end
+
+function connected:draw_debug_info()
+  if state.debug then
+    local fps = love.timer.getFPS()
+    local mem = collectgarbage("count")
+    local text = ("upd: %.2fms, drw: %.2fms, fps: %d, mem: %.2fMB, tex_mem: %.2fMB, ping: %.0fms"):format(
+                   connected.stats.time_update, connected.stats.time_draw, fps, mem / 1024,
+                   love.graphics.getStats().texturememory / 1024 / 1024, connection.server:round_trip_time())
+
+    love.graphics.setColor(255, 255, 255)
+    love.graphics.printf(text, 10, love.graphics.getHeight() - 20, love.graphics.getWidth(), "left")
+  end
+end
+
+function connected:leave()
+  connection.peer:disconnect()
+  connection.host:flush()
+end
+
+function connected:mousepressed(x, y, button, isTouch, presses)
+  for _, component in pairs(components) do component:mousepressed(x, y, button, isTouch, presses) end
+end
+
+function connected:mousereleased(x, y, button, isTouch, presses)
+  for _, component in pairs(components) do component:mousereleased(x, y, button, isTouch, presses) end
+end
+
+function love.update(dt)
+  -- always update timers
+  Timer.update(dt)
+end
+
+function love.keypressed(key, scancode, isrepeat)
+  if key == "escape" then love.event.quit() end
+end
+
 function love.quit()
-	print("Quitting client...")
-	if connection.peer then
-		connection.peer:disconnect()
-		connection.host:flush()
-	end
+  print("Quitting client...")
+  if connection.peer then
+    connection.peer:disconnect()
+    connection.host:flush()
+  end
 end
--- client/mfd.lua
+++ client/mfd.lua
@@ -1,66 +1,55 @@
-
 local layout = require("lib.suit.layout").new()
 local inspect = require("lib.inspect")
 
 -- MfdButton
 
 local MfdButton = Class {
-	size = 50,
-	lineOffset = 8,
-	isPressed = false,
-	sounds = {
-		pressed = love.audio.newSource("sounds/A/button-pressed.ogg", 'static'),
-		released = love.audio.newSource("sounds/A/button-released.ogg", 'static')
-	}
+  size = 50,
+  lineOffset = 8,
+  isPressed = false,
+  sounds = {
+    pressed = love.audio.newSource("sounds/A/button-pressed.ogg", "static"),
+    released = love.audio.newSource("sounds/A/button-released.ogg", "static"),
+  },
 }
 
 function MfdButton:init(mfd, id, x, y, w, h)
-	self.mfd = mfd
-	self.id = id
-	self.x = x
-	self.y = y
-	self.w = w
-	self.h = h
+  self.mfd = mfd
+  self.id = id
+  self.x = x
+  self.y = y
+  self.w = w
+  self.h = h
 end
 
 function MfdButton:draw()
-	if self.isPressed then
-		love.graphics.setColor(0.3,0.6,0.3)
-	else
-		love.graphics.setColor(0.3,0.3,0.3)
-	end
-	love.graphics.rectangle("fill", self.x, self.y, self.w, self.h, 5, 5)
-	love.graphics.setColor(.8,1,1)
-	love.graphics.rectangle("line", self.x + self.lineOffset, self.y + self.lineOffset, self.w - self.lineOffset*2, self.h - self.lineOffset*2, 3, 3)
+  if self.isPressed then
+    love.graphics.setColor(0.3, 0.6, 0.3)
+  else
+    love.graphics.setColor(0.3, 0.3, 0.3)
+  end
+  love.graphics.rectangle("fill", self.x, self.y, self.w, self.h, 5, 5)
+  love.graphics.setColor(.8, 1, 1)
+  love.graphics.rectangle("line", self.x + self.lineOffset, self.y + self.lineOffset, self.w - self.lineOffset * 2,
+                          self.h - self.lineOffset * 2, 3, 3)
 end
 
-function MfdButton:hit(x,y)
-	return x >= self.x and 
-		x <= self.x + self.w and 
-		y >= self.y and 
-		y <= self.y + self.h
+function MfdButton:hit(x, y)
+  return x >= self.x and x <= self.x + self.w and y >= self.y and y <= self.y + self.h
 end
 
 function MfdButton:pressed()
-	self.sounds.pressed:play()
-	self.isPressed = true
-	local message = {
-		type = "osb-pressed",
-		mfd = self.mfd,
-		osb = self.id
-	}
-	Signal.emit("send-to-server", message)
+  self.sounds.pressed:play()
+  self.isPressed = true
+  local message = {type = "osb-pressed", mfd = self.mfd, osb = self.id}
+  Signal.emit("send-to-server", message)
 end
 
 function MfdButton:released()
-	self.sounds.released:play()
-	self.isPressed = false
-	local message = {
-		type = "osb-released",
-		mfd = self.mfd,
-		osb = self.id
-	}
-	Signal.emit("send-to-server", message)
+  self.sounds.released:play()
+  self.isPressed = false
+  local message = {type = "osb-released", mfd = self.mfd, osb = self.id}
+  Signal.emit("send-to-server", message)
 end
 
 --- Mfd
@@ -68,85 +57,77 @@ end
 local Mfd = Class {}
 
 function Mfd:init(identifier, x, y)
-	self.id = identifier
-	self.position = {
-		x = x or 0,
-		y = y or 0
-	}
-	self.buttons = {}
-	self:createButtons(identifier)
+  self.id = identifier
+  self.position = {x = x or 0, y = y or 0}
+  self.buttons = {}
+  self:createButtons(identifier)
 end
 
 function Mfd:createButtons(identifier)
-	layout:reset(self.position.x, self.position.y):padding(20,20)
+  layout:reset(self.position.x, self.position.y):padding(20, 20)
 
-	-- top row of buttons
-	layout:push(self.position.x, self.position.y)
-	layout:col(MfdButton.size, MfdButton.size)
-	for _, osb in ipairs { "OSB1", "OSB2", "OSB3", "OSB4", "OSB5"} do
-		table.insert(self.buttons, MfdButton(identifier, osb, layout:col()))
-	end
+  -- top row of buttons
+  layout:push(self.position.x, self.position.y)
+  layout:col(MfdButton.size, MfdButton.size)
+  for _, osb in ipairs {"OSB1", "OSB2", "OSB3", "OSB4", "OSB5"} do
+    table.insert(self.buttons, MfdButton(identifier, osb, layout:col()))
+  end
 
-	-- right column
-	layout:right()
-	for _, osb in ipairs { "OSB6", "OSB7", "OSB8", "OSB9", "OSB10"} do
-		table.insert(self.buttons, MfdButton(identifier, osb, layout:row()))
-	end
+  -- right column
+  layout:right()
+  for _, osb in ipairs {"OSB6", "OSB7", "OSB8", "OSB9", "OSB10"} do
+    table.insert(self.buttons, MfdButton(identifier, osb, layout:row()))
+  end
 
-	-- left column
-	layout:pop()
-	layout:push(self.position.x, self.position.y)
-	layout:col(MfdButton.size, MfdButton.size)
-	for _, osb in ipairs { "OSB20", "OSB19", "OSB18", "OSB17", "OSB16"} do
-		table.insert(self.buttons, MfdButton(identifier, osb, layout:row()))
-	end
+  -- left column
+  layout:pop()
+  layout:push(self.position.x, self.position.y)
+  layout:col(MfdButton.size, MfdButton.size)
+  for _, osb in ipairs {"OSB20", "OSB19", "OSB18", "OSB17", "OSB16"} do
+    table.insert(self.buttons, MfdButton(identifier, osb, layout:row()))
+  end
 
-	-- bottom row
-	layout:down()
-	for _, osb in ipairs { "OSB15", "OSB14", "OSB13", "OSB12", "OSB11"} do
-		table.insert(self.buttons, MfdButton(identifier, osb, layout:col()))
-	end
+  -- bottom row
+  layout:down()
+  for _, osb in ipairs {"OSB15", "OSB14", "OSB13", "OSB12", "OSB11"} do
+    table.insert(self.buttons, MfdButton(identifier, osb, layout:col()))
+  end
 end
 
 function Mfd:draw()
-	for _,button in ipairs(self.buttons) do
-		button:draw()
-	end
-	if self.imageData then
-		love.graphics.setColor(1,1,1)
-		local image = love.graphics.newImage(self.imageData)
-		love.graphics.draw(image, self.position.x + MfdButton.size, self.position.y + MfdButton.size, 0, 0.85, 0.85)
-	else
-		-- draw no data string here.
-	end
+  for _, button in ipairs(self.buttons) do button:draw() end
+  if self.imageData then
+    love.graphics.setColor(1, 1, 1)
+    local image = love.graphics.newImage(self.imageData)
+    love.graphics.draw(image, self.position.x + MfdButton.size, self.position.y + MfdButton.size, 0, 0.85, 0.85)
+  else
+    -- draw no data string here.
+  end
 end
 
 function Mfd:update(dt)
-	local message = {
-		type = "streamed-texture",
-		identifier = self.id
-	}
-	Signal.emit("send-to-server", message)
+  local message = {type = "streamed-texture", identifier = self.id}
+  Signal.emit("send-to-server", message)
 end
 
 function Mfd:consume(data)
-	self.imageData = love.image.newImageData(love.data.newByteData(data))
+  self.imageData = love.image.newImageData(love.data.newByteData(data))
 end
 
 function Mfd:mousepressed(x, y, button, isTouch, presses)
-	for _,button in ipairs(self.buttons) do
-		if button:hit(x,y) then
-			self.pressed = button
-			button:pressed()
-		end
-	end
+  for _, button in ipairs(self.buttons) do
+    if button:hit(x, y) then
+      self.pressed = button
+      button:pressed()
+    end
+  end
 end
 
 function Mfd:mousereleased(x, y, button, isTouch, presses)
-	if self.pressed then
-		self.pressed:released()
-		self.pressed = nil
-	end
+  if self.pressed then
+    self.pressed:released()
+    self.pressed = nil
+  end
 end
 
 return Mfd
