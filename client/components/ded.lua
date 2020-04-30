0444c41365299c9aa90b2757e9265ffb05161e4f
Silvio Heuberger <silvio.heuberger@beekeeper.io>
2020-04-30 11:52:01 +0200

    Reformat lua code

--- client/components/ded.lua
+++ client/components/ded.lua
@@ -1,36 +1,36 @@
 local Ded = Class {
-    image = nil, -- drawable image
-    imageData = nil
+  image = nil, -- drawable image
+  imageData = nil,
 }
 
 function Ded:init(id, x, y)
-    self.id = id
-    self.position = {x = x or 0, y = y or 0}
+  self.id = id
+  self.position = {x = x or 0, y = y or 0}
 end
 
 function Ded:update(dt)
 end
 
 function Ded:draw()
-    if self.imageData then
-        love.graphics.setColor(1, 1, 1)
-        if not self.image then
-          -- only load image if it is a new frame.
-          self.image = love.graphics.newImage(self.imageData)
-        end
-        love.graphics.draw(self.image, self.position.x, self.position.y, 0, 0.85, 0.85)
-      else
-        -- TODO: draw no data string here.
-      end
+  if self.imageData then
+    love.graphics.setColor(1, 1, 1)
+    if not self.image then
+      -- only load image if it is a new frame.
+      self.image = love.graphics.newImage(self.imageData)
+    end
+    love.graphics.draw(self.image, self.position.x, self.position.y, 0, 0.85, 0.85)
+  else
+    -- TODO: draw no data string here.
+  end
 end
 
 function Ded:consume(data)
-    self.imageData = love.image.newImageData(love.data.newByteData(data))
-    self.image = nil
-  end
+  self.imageData = love.image.newImageData(love.data.newByteData(data))
+  self.image = nil
+end
 
 function Ded:mousepressed(x, y, button, isTouch, presses)
-    -- intentionally left blank
+  -- intentionally left blank
 end
 
 function Ded:mousereleased(x, y, button, isTouch, presses)
--- client/icp.lua
+++ client/icp.lua
@@ -8,20 +8,24 @@ local StreamedTexture = require("util.streamed-texture")
 
 local components = {}
 
-local icp = {components = components, stats = {}, channels = {
-  -- general purpose reliable channel
-  [0] = function(event)
-    local payload = msgpack.unpack(event.data)
-    print("Received general purpose event: ", inspect(payload))
-  end,
-  -- texture memory channels are unrealiable and pure image data.
-  [3] = function(event)
-    components["f16/ded"]:consume(event.data)
-  end,
-  [4] = function(event)
-    components["f16/rwr"]:consume(event.data)
-  end,
-}}
+local icp = {
+  components = components,
+  stats = {},
+  channels = {
+    -- general purpose reliable channel
+    [0] = function(event)
+      local payload = msgpack.unpack(event.data)
+      print("Received general purpose event: ", inspect(payload))
+    end,
+    -- texture memory channels are unrealiable and pure image data.
+    [3] = function(event)
+      components["f16/ded"]:consume(event.data)
+    end,
+    [4] = function(event)
+      components["f16/rwr"]:consume(event.data)
+    end,
+  },
+}
 
 function icp:init()
   self.components["f16/ded"] = Ded("f16/ded", 20, 30)
@@ -48,9 +52,7 @@ end
 
 function icp:handleReceive(event)
   local handler = self.channels[event.channel]
-  if handler then
-    handler(event)
-  end
+  if handler then handler(event) end
 end
 
 function icp:draw()
