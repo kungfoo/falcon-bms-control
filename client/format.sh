fa1355892dd4a9859d8bffeff94bd1cc37c44296
Silvio Heuberger <silvio.heuberger@ergon.ch>
2020-11-03 09:27:16 +0100

    Make formatting lua code automagic with new files

--- client/format.sh
+++ client/format.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-lua-format -vi main.lua \
-    mfds.lua icp.lua \
-    components/icp.lua components/icp-button.lua \
-    components/mfd-button.lua components/mfd.lua \
-    components/switcher.lua components/ded.lua
+find . -name '*.lua' -exec lua-format -vi {} \;
+
