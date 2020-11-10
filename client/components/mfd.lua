051c87eac57d2e3fbd394e69cede3d81994a7de6
Silvio Heuberger <silvio.heuberger@ergon.ch>
2020-11-10 16:48:19 +0100

    Use transform object to properly scale and translate screen space coordinates

--- client/components/mfd.lua
+++ client/components/mfd.lua
@@ -7,9 +7,8 @@ local Mfd = Class {}
 
 function Mfd:init(identifier, x, y)
   self.id = identifier
-  self.position = {x = x or 0, y = y or 0}
-  self.dimensions = {w = 500, h = 500}
   self.buttons = {}
+  self.transform = love.math.newTransform()
   self:createButtons(identifier)
 end
 
@@ -17,27 +16,27 @@ function Mfd:createButtons(identifier)
   self.buttons = {}
 
   -- top row of buttons
-  layout:push(self.position.x + 105, self.position.y):padding(61 - MfdButton.size, 0)
+  layout:push(105, 0):padding(61 - MfdButton.size, 0)
   for _, osb in ipairs {"OSB_1", "OSB_2", "OSB_3", "OSB_4", "OSB_5"} do
     table.insert(self.buttons, MfdButton(identifier, osb, layout:col(MfdButton.size, MfdButton.size)))
   end
 
   -- right column
   layout:pop()
-  layout:push(self.position.x + MfdButton.size + 410, self.position.y + 110):padding(0, 58 - MfdButton.size)
+  layout:push(MfdButton.size + 410, 110):padding(0, 58 - MfdButton.size)
   for _, osb in ipairs {"OSB_6", "OSB_7", "OSB_8", "OSB_9", "OSB_10"} do
     table.insert(self.buttons, MfdButton(identifier, osb, layout:row(MfdButton.size, MfdButton.size)))
   end
 
   -- left column
-  layout:push(self.position.x, self.position.y + 110):padding(0, 58 - MfdButton.size)
+  layout:push(0, 110):padding(0, 58 - MfdButton.size)
   for _, osb in ipairs {"OSB_20", "OSB_19", "OSB_18", "OSB_17", "OSB_16"} do
     table.insert(self.buttons, MfdButton(identifier, osb, layout:row(MfdButton.size, MfdButton.size)))
   end
 
   -- bottom row
   layout:pop()
-  layout:push(self.position.x + 105, self.position.y + 410 + MfdButton.size):padding(61 - MfdButton.size, 0)
+  layout:push(105, 410 + MfdButton.size):padding(61 - MfdButton.size, 0)
   for _, osb in ipairs {"OSB_15", "OSB_14", "OSB_13", "OSB_12", "OSB_11"} do
     table.insert(self.buttons, MfdButton(identifier, osb, layout:col(MfdButton.size, MfdButton.size)))
   end
@@ -45,12 +44,11 @@ end
 
 function Mfd:draw()
   -- scale to fit everything
-  local scale = self:determineScale()
   love.graphics.push()
-  love.graphics.scale(scale, scale)
+  love.graphics.applyTransform(self.transform)
 
-  love.graphics.setColor(Colors.grey)
-  love.graphics.rectangle("line", self.position.x, self.position.y, self.dimensions.w, self.dimensions.h, 0, 0)
+  -- love.graphics.setColor(Colors.green)
+  -- love.graphics.rectangle("line", 0, 0, 500, 500, 0, 0)
 
   for _, button in ipairs(self.buttons) do button:draw() end
   if self.imageData then
@@ -61,7 +59,7 @@ function Mfd:draw()
     end
     -- TODO: scale image according to component width and height, not a fixed scale.
     local texture_scale = 410 / self.image:getWidth()
-    love.graphics.draw(self.image, self.position.x + MfdButton.size, self.position.y + MfdButton.size, 0, texture_scale, texture_scale)
+    love.graphics.draw(self.image, MfdButton.size,MfdButton.size, 0, texture_scale, texture_scale)
   else
     -- TODO: draw no data string here.
   end
@@ -69,12 +67,12 @@ function Mfd:draw()
   love.graphics.pop()
 end
 
-function Mfd:determineScale()
-  if self.dimensions.w >= 500 and self.dimensions.w >= 500 then
+function Mfd:determineScale(w,h)
+  if w >= 500 and h >= 500 then
     -- do not scale up
     return 1.0
   else
-    local d = math.min(self.dimensions.w, self.dimensions.h)
+    local d = math.min(w,h)
     return d / 500
   end
 end
@@ -83,10 +81,9 @@ function Mfd:update(dt)
 end
 
 function Mfd:updateGeometry(x, y, w, h)
-  self.position.x = x
-  self.position.y = y
-  self.dimensions.w = w
-  self.dimensions.h = h
+  local scale = self:determineScale(w, h)
+  self.transform = love.math.newTransform()
+  self.transform:translate(x,y):scale(scale)
   self:createButtons()
 end
 
@@ -104,8 +101,10 @@ function Mfd:stop()
 end
 
 function Mfd:mousepressed(x, y, button, isTouch, presses)
+  local dx, dy = self.transform:inverseTransformPoint(x,y)
+  print("Relative point: ", dx, dy)
   for _, button in ipairs(self.buttons) do
-    if button:hit(x, y) then
+    if button:hit(dx, dy) then
       self.pressed = button
       button:pressed()
     end
