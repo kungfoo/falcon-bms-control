f12a713c65f2b9abec8141f9061f0b2a8faf7fa8
Silvio Heuberger <silvio.heuberger@gmail.com>
2020-11-30 17:39:18 +0100

    Fix setColor() missing .graphics

--- client/main.lua
+++ client/main.lua
@@ -88,7 +88,7 @@ function broadcasting:leave()
 end
 
 function broadcasting:draw()
-  love.setColor(Colors.white)
+  love.graphics.setColor(Colors.white)
   love.graphics.setFont(font)
   love.graphics.print("DISCOVERING SERVER" .. shine.dots[shine.position], 30, 30)
 end
