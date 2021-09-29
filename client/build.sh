3aa7c14832b281519bb9500086f19436807fd94a
Silvio Heuberger <silvio.heuberger@gmail.com>
2021-09-29 22:45:48 +0200

    Download love 11.3 before building using boon

--- client/build.sh
+++ client/build.sh
@@ -4,4 +4,7 @@ pushd ../boon || exit
 cargo build --release
 popd || exit
 
+../boon/target/release/boon love download 11.3
 ../boon/target/release/boon build . --version 11.3
+
+mv release/Falcon\ Bms\ Control.love release/game.love
